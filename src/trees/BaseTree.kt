4455c9df847633671613c50612e3de9a5417d7f7
bikakuka <bikakukaVN@gmail.com>
2024-03-30 06:35:59 +0300

    Rename functions and replace open to abstract funcs
    Do this becouse we didn't have in plans to write any working code to this template.
    
    We want to realize all by yourself

---
+++ src/trees/BaseTree.kt
@@ -0,0 +1,17 @@
+package trees
+
+import trees.nodes.BaseNode
+
+abstract class BaseTree<T: Comparable<T>, N: BaseNode<T, N>>{
+    var root : N? = null
+
+    abstract fun createNode(data: T) : N
+
+    abstract fun add(data: T) : N
+
+    abstract fun find(data: T) : N?
+
+    abstract fun delete(data: T)
+
+    abstract fun print()
+}
