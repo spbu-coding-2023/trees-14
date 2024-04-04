349eab9a30686ef3b0718c462b219c1ae8ac45c5
Matvey Nazdruhin <144718766+bikakuka@users.noreply.github.com>
2024-04-05 01:05:46 +0300

    add realisation for BSTree
    Hopes it is near the end..
    
    so add fully realisation of BSTree (without any balancer)

--- src/trees/binarySearchTree.kt
+++ src/trees/binarySearchTree.kt
@@ -1,28 +1,145 @@
 package trees
 
-import trees.nodes.BaseNode
 import trees.nodes.BinarySearchNode
 
 
-class binarySearchTree<T: Comparable<T>>(data: T) : BaseTree<T, BinarySearchNode<T>>(){
-    //что то архитектурное происходит (самый цензурный вариант)
-    override fun createNode(data: T): BinarySearchNode<T> {
-        TODO("Not yet implemented")
-    }
+class binarySearchTree<T : Comparable<T>> : BaseTree<T, BinarySearchNode<T>>() {
+    override var root: BinarySearchNode<T>? = null
+    override fun createNode(data: T): BinarySearchNode<T> = BinarySearchNode(data)
 
     override fun add(data: T): BinarySearchNode<T> {
-        TODO("Not yet implemented")
+        if (root == null) {
+            root = createNode(data)
+            return root!!
+        }
+        val newNode = createNode(data)
+        var curNode = root
+        while (true) {
+            if (curNode == null)
+                return newNode
+            if (curNode.data > data) {
+                if (curNode.left == null) {
+                    curNode.left = newNode
+                    newNode.parent = curNode
+                    return newNode
+                } else
+                    curNode = curNode.left
+            } else {
+                if (curNode.right == null) {
+                    curNode.right = newNode
+                    newNode.parent = curNode
+                    return newNode
+                } else
+                    curNode = curNode.right
+            }
+
+        }
     }
 
     override fun find(data: T): BinarySearchNode<T>? {
-        TODO("Not yet implemented")
+        if (root == null) {
+            return null
+        }
+        var curNode = root
+        while (true) {
+
+            if (curNode!!.data > data) {
+                if (curNode.left != null)
+                    curNode = curNode.left
+                else
+                    return null
+            }
+            if (curNode!!.data < data) {
+                if (curNode.right != null)
+                    curNode = curNode.right
+                else
+                    return null
+            } else
+                return curNode
+        }
     }
 
-    override fun delete(data: T) {
-        TODO("Not yet implemented")
+    override fun delete(data: T): BinarySearchNode<T>? {
+        val node = find(data) ?: return null
+        if (node.right == null && node.left == null) {
+            if (node.parent == null) {
+                root = null
+            } else if (node.parent!!.data > data) {
+                node.parent!!.left = null
+            } else
+                node.parent!!.right = null
+            return node
+        } else if (node.right != null && node.left != null) {
+            val righ = node.right // >node
+            val lef = node.left // < node
+            if (node.parent == null) {
+                root = righ
+                var curNode = root
+                while (true) {
+                    if (curNode!!.left == null) {
+                        curNode.left = lef
+                        break
+                    } else
+                        curNode = curNode.left
+                }
+            } else {
+                if (node.parent!!.data > data) {
+                    node.parent!!.left = righ
+                    var curNode = righ
+                    while (true) {
+                        if (curNode!!.left == null) {
+                            curNode.left = lef
+                            break
+                        } else
+                            curNode = curNode.left
+                    }
+                } else {
+                    node.parent!!.right = righ
+                    var curNode = righ
+                    while (true) {
+                        if (curNode!!.left == null) {
+                            curNode.left = lef
+                            break
+                        } else
+                            curNode = curNode.left
+                    }
+                }
+            }
+
+        } else if (node.right != null || node.left != null) {
+            if (node.parent == null) {
+                root = node.left ?: node.right
+                return node
+            } else if (node.right != null) {
+                if (node.parent!!.data > data)
+                    node.parent!!.left = node.right
+                else
+                    node.parent!!.right = node.right
+            } else {
+                if (node.parent!!.data > data)
+                    node.parent!!.left = node.left
+                else
+                    node.parent!!.right = node.left
+            }
+        }
+        return node
     }
 
-    override fun print() {
-        TODO("Not yet implemented")
+    var out = mutableListOf<T>()
+    override fun iterator(): MutableList<T> {
+        out = mutableListOf<T>()
+        inOrder(root)
+        return out
     }
+
+    private fun inOrder(root: BinarySearchNode<T>?) {
+        if (root == null) {
+            return
+        }
+
+        if (root.left != null) inOrder(root.left)
+        out.add(root.value)
+        if (root.right != null) inOrder(root.right)
+    }
+
 }
