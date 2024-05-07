12c7836a733948abba1e5338645da4418ca8b99d
bikakuka <bikakukaVN@gmail.com>
2024-05-08 02:52:11 +0300

    Add more tests
    add tests for some unobvious cases :)

--- src/test/kotlin/AVLTest.kt
+++ src/test/kotlin/AVLTest.kt
@@ -1,13 +1,16 @@
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import src.trees.AVLTree
+import src.trees.nodes.AVLNode
 
 class AVLTest {
+    //coverage with JaCoCo - https://disk.yandex.ru/i/GFDp0L3AScO1CQ
     @Test
     //a test to check the structure of the resulting tree
     //an img of tree - https://disk.yandex.ru/i/2r6Fza70QbaQpg
     fun testCheckStructOfTree() {
         val tree: AVLTree<Int> = AVLTree()
+        tree.add(10, 1)
         tree.add(10, 1)
         tree.add(15, 1)
         tree.add(5, 1)
@@ -29,6 +32,29 @@ class AVLTest {
         b.add(Pair(15, 1))
         b.add(Pair(20, 1))
         assertEquals(a, b)
+    }
+
+    @Test
+    //a test to check the removal from non-existent node from a tree
+    fun testDeleteUnExistenNode() {
+        val tree: AVLTree<Int> = AVLTree()
+        tree.add(10, 1)
+        tree.add(15, 1)
+        tree.add(5, 1)
+        tree.add(20, 1)
+        tree.add(13, 1)
+        tree.add(6, 1)
+        tree.add(2, 1)
+        tree.add(0, 1)
+        tree.add(11, 1)
+        assertEquals(null, tree.delete(100))
+    }
+
+    @Test
+    //a test to check the removal from non-existent node from an empty tree
+    fun testDeleteNodeFromEmptyTree() {
+        val tree: AVLTree<Int> = AVLTree()
+        assertEquals(null, tree.delete(100))
     }
 
     @Test
@@ -175,4 +201,58 @@ class AVLTest {
         assertEquals(a, b)
     }
 
+    @Test
+    //a test to verify the operation of the search for a non-existent node by key
+    fun testSearchNotExistNodeByKey() {
+        val tree: AVLTree<Int> = AVLTree()
+        tree.add(10, 1)
+        tree.add(15, 1)
+        tree.add(5, 1)
+        tree.add(20, 1)
+        tree.add(13, 1)
+        tree.add(6, 1)
+        tree.add(2, 1)
+        tree.add(0, 1)
+        tree.add(11, 1)
+        val q = tree.find(12)
+        assertEquals(null ,q)
+    }
+
+    @Test
+    //a test to verify the operation of the search for an exist node by key
+    fun testSearchNodeByKey() {
+        val tree: AVLTree<Int> = AVLTree()
+        tree.add(10, 1)
+        tree.add(15, 1)
+        tree.add(5, 1)
+        tree.add(20, 1)
+        tree.add(13, 1)
+        tree.add(6, 1)
+        tree.add(2, 1)
+        tree.add(0, 1)
+        tree.add(11, 1)
+        val q = tree.find(13)
+        assertEquals(true, q!!.key == 13 && q.value == 1)
+    }
+
+    @Test
+    //a test to check the presence and absence of a node in the tree by key
+    fun testIsNodeInTree() {
+        val tree: AVLTree<Int> = AVLTree()
+        tree.add(10, 1)
+        tree.add(15, 1)
+        tree.add(5, 1)
+        tree.add(20, 1)
+        tree.add(13, 1)
+        tree.add(6, 1)
+        tree.add(2, 1)
+        tree.add(0, 1)
+        tree.add(11, 1)
+        assertEquals(true,  !tree.isInTree(12) && tree.isInTree(13))
+    }
+    @Test
+    fun testPrintEmptyTree(){
+        val tree: AVLTree<Int> = AVLTree()
+        assertEquals(mutableListOf<Pair<Int,Any>>(), tree.iter())
+    }
 }
