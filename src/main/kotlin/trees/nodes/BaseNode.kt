4821b346e460e99c3b750941a87873739019054a
Matvey Nazdruhin <144718766+bikakuka@users.noreply.github.com>
2024-05-08 03:20:23 +0300

    Update BaseNode.kt
    update it for working RBTree

--- src/main/kotlin/trees/nodes/BaseNode.kt
+++ src/main/kotlin/trees/nodes/BaseNode.kt
@@ -1,7 +1,7 @@
 package src.trees.nodes
 
-abstract class BaseNode< T : Comparable<T>, N : BaseNode<T, N>>(private var k: T, var value: Any?){
-    var key? : T = k
+abstract class BaseNode< T : Comparable<T>, N : BaseNode<T, N>>(private var k: T? = null, var value: Any? = null){
+    var key : T? = k
     var left : N? = null
     var right : N? = null
     var parent : N? = null
