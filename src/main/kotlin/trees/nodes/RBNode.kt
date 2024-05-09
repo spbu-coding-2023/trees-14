211e6d880083b2475aff636edeb5308ef597d3c1
oleg01228 <olegbananas@gmail.com>
2024-05-09 22:56:04 +0300

    add tests and some fixes

--- src/main/kotlin/trees/nodes/RBNode.kt
+++ src/main/kotlin/trees/nodes/RBNode.kt
@@ -1,10 +1,8 @@
 package trees.nodes
 
-class RBNode<T: Comparable<T>> {
-    var key: T? = null
+import src.trees.nodes.BaseNode
+
+class RBNode<T : Comparable<T>>(k: T? = null, data: Any? = null) : BaseNode<T, RBNode<T>>(k, data) {
     var data: Any? = null
-    var parent: RBNode<T>? = null
-    var left: RBNode<T>? = null
-    var right: RBNode<T>? = null
     var color: Int = 0
 }
